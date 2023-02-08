8897276c149e95be476991c1aea27d39be5b9591
Max Base <maxbasecode@gmail.com>
2023-02-08 11:28:47 +0000

    handling args

--- UnlimitedAutoTranslate.py
+++ UnlimitedAutoTranslate.py
@@ -15,3 +15,24 @@ def split_text(text, max_len=4500):
         chunks.append(chunk)
     return chunks
 
+args = sys.argv
+
+# Handling the case when no file is passed
+if len(args) == 1:
+    print("Please pass a file to translate")
+    sys.exit(1)
+elif len(args) == 2:
+    print("Please pass a target language")
+    sys.exit(1)
+elif len(args) > 3:
+    text = str(open(args[1], encoding='utf8', newline='\n').read()).strip()
+
+    # Handling `<filename> <target>`
+    if len(args) == 3:
+        translated = translate(text, None, args[2])
+        print(translated)
+    # Handling `<filename> <from> <target>`
+    elif len(args) == 4:
+        translated = translate(text, args[2], args[3])
+        print(translated)
+
